d791c9771f5a9c6028a52d1a2e1fc271176c83ff
anod <alex.gavrishev@gmail.com>
2013-09-19 13:04:07 +0300

    Add acra error reporting

--- src/com/anod/appwatcher/AppWatcherApplication.java
+++ src/com/anod/appwatcher/AppWatcherApplication.java
@@ -7,12 +7,22 @@ import java.lang.reflect.Field;
 
 import com.newrelic.agent.android.NewRelic;
 
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
 
+@ReportsCrashes(
+	formKey = "", // This is required for backward compatibility but not used
+	formUri = "https://anodsplace.info/acra/report/report.php",
+	httpMethod = org.acra.sender.HttpSender.Method.PUT,
+	reportType = org.acra.sender.HttpSender.Type.JSON
+)
 public class AppWatcherApplication extends Application {
 
 	 @Override
 	 public void onCreate() {
 		super.onCreate();
+
+		ACRA.init(this);
 
 		 NewRelic.withApplicationToken(
 			"AA47c4b684f2af988fdf3a13518738d7eaa8a4976f"
@@ -30,4 +40,7 @@ public class AppWatcherApplication extends Application {
 		 }
 
 	 }
+
+
+
 }
