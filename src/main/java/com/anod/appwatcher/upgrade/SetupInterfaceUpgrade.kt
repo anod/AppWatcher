59c5c99d2286ddb78e246c8be7ab04a2a56da506
Alex Gavrishev <alex.gavrishev@gmail.com>
2018-03-10 10:58:10 +0200

    Version 92, init upgrade dialog switches

--- src/main/java/com/anod/appwatcher/upgrade/SetupInterfaceUpgrade.kt
+++ src/main/java/com/anod/appwatcher/upgrade/SetupInterfaceUpgrade.kt
@@ -3,8 +3,8 @@ package com.anod.appwatcher.upgrade
 import android.content.Context
 import android.content.Intent
 import android.widget.Switch
-import com.anod.appwatcher.AppWatcherActivity
 import com.anod.appwatcher.R
+import com.anod.appwatcher.AppWatcherActivity
 import com.anod.appwatcher.preferences.Preferences
 import com.anod.appwatcher.utils.DialogCustom
 
@@ -21,18 +21,26 @@ class SetupInterfaceUpgrade(val prefs: Preferences, val context: Context): Upgra
         }
 
         DialogCustom(context, R.string.setup_interface_title, R.layout.dialog_setup_interface, { view, builder ->
+                var recreate = false
+                view.findViewById<Switch>(R.id.recentToggle).isChecked = prefs.showRecent
+                view.findViewById<Switch>(R.id.onDeviceToggle).isChecked = prefs.showOnDevice
+
                 view.findViewById<Switch>(R.id.recentToggle).setOnCheckedChangeListener({ _, checked ->
                     prefs.showRecent = checked
+                    recreate = true
                 })
 
                 view.findViewById<Switch>(R.id.onDeviceToggle).setOnCheckedChangeListener({ _, checked ->
                     prefs.showOnDevice = checked
+                    recreate = true
                 })
 
                 builder.setPositiveButton(android.R.string.ok, { dialog, which ->
-                    val i = Intent(context, AppWatcherActivity::class.java)
-                    i.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    context.startActivity(i)
+                    if (recreate) {
+                        val i = Intent(context, AppWatcherActivity::class.java)
+                        i.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        context.startActivity(i)
+                    }
                     dialog.dismiss()
                 })
         }).show()
