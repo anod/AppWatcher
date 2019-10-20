e1b65f708c068af1d83d449936b07c17ea798aa2
Alex Gavrishev <alex.gavrishev@gmail.com>
2019-10-20 16:29:40 +0300

    Build 132

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.anod.appwatcher"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 131
+        versionCode = 132
         versionName = "1.3"
         resValue("string", "content_authority", "com.anod.appwatcher")
 
