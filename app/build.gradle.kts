a823913253d499451c7679c43ee89ddd989996a0
Alexander Gavrishev <algavris@microsoft.com>
2022-09-03 20:56:34 +0300

    15010

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.anod.appwatcher"
         minSdk = 27
         targetSdk = 33
-        versionCode = 15009
+        versionCode = 15010
         versionName = "1.5.0"
         resValue("string", "content_authority", "com.anod.appwatcher")
 
