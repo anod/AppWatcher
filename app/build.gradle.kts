3856ca745b323b9623be48a04ab248e87f24939c
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2023-01-28 13:04:45 +0200

    16003

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.anod.appwatcher"
         minSdk = 27
         targetSdk = 33
-        versionCode = 16002
+        versionCode = 16003
         versionName = "1.6.0"
     }
 
