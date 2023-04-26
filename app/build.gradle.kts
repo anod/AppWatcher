1133c0b152c06124044fea87c547b87134db3ffa
Alexandr Gavrishev <alex.gavrishev@gmail.com>
2023-04-26 18:11:07 +0300

    Version 16102

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.anod.appwatcher"
         minSdk = 27
         targetSdk = 33
-        versionCode = 16101
+        versionCode = 16102
         versionName = "1.6.1"
     }
 
