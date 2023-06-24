dc424e98e98fa502f70334053c254a6c262a6540
anod <alex.gavrishev@gmail.com>
2023-06-24 14:42:24 +0300

    Update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.anod.appwatcher"
         minSdk = 27
         targetSdk = 33
-        versionCode = 16107
+        versionCode = 16108
         versionName = "1.6.1"
     }
 
